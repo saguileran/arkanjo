824879f7e9f86307ceb4c719fbf2862bf7620f2e
sebas <>
2025-05-08 16:48:46 -0300

    Fixing low contrast between light terminal backgrounds with program font colors. The aditional code added implements a method to verify the terminal background color based on the oeprating system. Then, based on the resulting mean of the terminal background colors a colors set is defined between a dark and light themes, where the colors are opposed.

--- arkanjo/base/utils.cpp
+++ arkanjo/base/utils.cpp
@@ -77,7 +77,13 @@ bool Utils::is_regular_file(string path){
 }
 
 string Utils::format_colored_message(string message, COLOR color){
-	return COLOR_TOKENS_UTILS[color] + message + COLOR_TOKENS_UTILS[RESET];
+	if (is_bg_color_dark()){
+		return COLOR_TOKENS_UTILS_LIGTH[color] + message + COLOR_TOKENS_UTILS_LIGTH[RESET];
+	}
+	else{
+	return COLOR_TOKENS_UTILS_DARK[color] + message + COLOR_TOKENS_UTILS_DARK[RESET];
+
+	}
 }
 
 bool Utils::is_empty_char(char c){
@@ -123,4 +129,131 @@ vector<string> Utils::split_string(string s, char delimiter){
 		ret.push_back(cur_token);
 	}
 	return ret;
+}
+
+int Utils::convert_16_bit_to_8_bit(const std::string& hex16) {
+  try {
+      unsigned long value = std::stoul(hex16, nullptr, 16);
+      return static_cast<int>(value / 256);  
+  } catch (...) {
+      return 0; 
+  }
+}
+
+tuple<int, int, int> Utils::parse_terminal_color_response(const string& response) {
+  // std::string start_pos;
+  const size_t start_pos = response.find("rgb:");
+  if (start_pos == string::npos) {
+      return {0, 0, 0};  // Return black if format not found
+  }
+
+  const size_t end_pos = response.find("\033\\", start_pos);
+  if (end_pos == string::npos) {
+      return {0, 0, 0};
+  }
+
+  string rgb_str = response.substr(start_pos + 4, end_pos - (start_pos + 4));
+
+  replace(rgb_str.begin(), rgb_str.end(), '/', ' ');
+
+  istringstream iss(rgb_str);
+  string r_hex, g_hex, b_hex;
+  iss >> r_hex >> g_hex >> b_hex;
+
+  int r = convert_16_bit_to_8_bit(r_hex);
+  int g = convert_16_bit_to_8_bit(g_hex);
+  int b = convert_16_bit_to_8_bit(b_hex);
+
+  return {r, g, b};
+}
+
+string Utils::capture_terminal_response() {
+  termios oldt, newt;
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+  int oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+  cout << "\033]11;?\033\\";
+  cout.flush();
+
+  string response;
+  char ch;
+  timeval tv;
+  tv.tv_sec = 0;
+  tv.tv_usec = 100000; // 100ms timeout
+
+  fd_set readfds;
+  FD_ZERO(&readfds);
+  FD_SET(STDIN_FILENO, &readfds);
+
+  while (select(STDIN_FILENO + 1, &readfds, NULL, NULL, &tv) > 0) {
+      if (read(STDIN_FILENO, &ch, 1) > 0) {
+          response += ch;
+      } else {
+          break;
+      }
+  }
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+  return response;
+}
+
+int Utils::get_terminal_bg_color_mean() {
+  if (!isatty(STDOUT_FILENO)) {
+      return 0;
+  }
+
+  string color_str = capture_terminal_response();
+  auto [r, g, b] = parse_terminal_color_response(color_str);
+  int mean_color = (r+g+b)/3;
+  return mean_color;
+}
+
+bool Utils::is_bg_color_dark(){
+	#ifdef _WIN32
+			CONSOLE_SCREEN_BUFFER_INFOEX csbi;
+			HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+			if (GetConsoleScreenBufferInfoEx(hConsole, &csbi)) {
+					COLORREF backgroundColor = csbi.ColorTable[csbi.wAttributes & 0x0F];
+
+					int r = GetRValue(backgroundColor);
+					int g = GetGValue(backgroundColor);
+					int b = GetBValue(backgroundColor);
+
+					if ((r+g+b)/3 < 128){
+							return true;
+					}
+					else{
+							return false;
+					}
+			} else {
+					std::cerr << "Error getting console screen buffer info." << std::endl;
+					return true;
+			}
+	#elif __linux__
+			if (get_terminal_bg_color_mean() < 128){
+					return true;
+			}
+			else{
+					return false;
+			}
+
+	#elif __APPLE__
+			if (get_terminal_bg_color_mean() < 128){
+				return true;
+		}
+		else{
+				return false;
+		}
+	#else
+			return true;
+	#endif
+
 }
--- arkanjo/base/utils.hpp
+++ arkanjo/base/utils.hpp
@@ -8,6 +8,26 @@
 #include <jsoncpp/json/json.h>
 #include <sys/stat.h>
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cstdlib>
+#include <unistd.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+
+#ifdef _WIN32
+    #include <windows.h>
+#elif __linux__
+    #include <sys/utsname.h>
+#elif __APPLE__
+    #include <TargetConditionals.h>
+#endif
+
 using namespace std;
 
 
@@ -17,18 +37,32 @@ namespace Utils{
 
 	const int MKDIR_FLAG = 0700;
 
-	const vector<string> COLOR_TOKENS_UTILS = {
-		"\033[0m", //RESET
-		"\033[31m", //RED
-		"\033[32m", //GREEN
-		"\033[33m", //YELLOW
-		"\033[34m", //BLUE
-		"\033[35m", //MAGENTA
-		"\033[36m", //CYAN
-		"\033[37m", //GRAY
-		"\033[97m", //WHITE
+	const vector<string> COLOR_TOKENS_UTILS_LIGTH = {
+		"\033[0m",    //RESET
+		"\033[31m",   //RED
+		"\033[32m",   //GREEN
+		"\033[33m",   //YELLOW
+		"\033[34m",   //BLUE
+		"\033[35m",   //MAGENTA
+		"\033[36m",   //CYAN
+		"\033[37m",   //GRAY
+		"\033[97m",   //WHITE
 		"\033[33;1m", //BRIGHT_YELLOW
-		"\033[30m", //BLACK
+		"\033[30m",   //BLACK
+	};
+
+  const vector<string> COLOR_TOKENS_UTILS_DARK = {
+		"\033[0m",    // RESET
+    "\033[31;2m", // DARK_RED
+    "\033[32;2m", // DARK_GREEN
+    "\033[33;2m", // DARK_YELLOW
+    "\033[34;2m", // DARK_BLUE
+    "\033[35;2m", // DARK_MAGENTA
+    "\033[36;2m", // DARK_CYAN
+    "\033[37;2m", // DAGRAYRK_GRAY
+    "\033[30m",   // BLACK
+    "\033[33m",   // YELLOW
+    "\033[97m"    // WHITE
 	};
 	
 	enum COLOR{
@@ -59,7 +93,16 @@ namespace Utils{
 	bool is_special_char(char c);
 
 	vector<string> split_string(string s, char delimiter);
+
+  int convert_16_bit_to_8_bit(const string& hex16);
+  tuple<int, int, int> parse_terminal_color_response(const string& response);
+  string capture_terminal_response();
+  bool is_bg_dark();
+  string capture_terminal_response();
+  bool is_bg_color_dark();
+	int get_terminal_bg_color_mean();
 };
 
-#endif
 
+
+#endif
